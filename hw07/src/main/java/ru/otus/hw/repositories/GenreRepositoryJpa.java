410eb26e46ec6fb527465fdbe7676981dfa09b33
Marat Bekyashev <MBekyashev@sportmasterlab.net>
2025-11-22 16:27:02 +0300

    ДЗ №7 Инициализация на базе ДЗ №6

---
+++ hw07/src/main/java/ru/otus/hw/repositories/GenreRepositoryJpa.java
@@ -0,0 +1,38 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class GenreRepositoryJpa implements GenreRepository {
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Genre> findAll() {
+        TypedQuery<Genre> query = entityManager.createQuery("select g from Genre g", Genre.class);
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Genre> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Genre> findAllByIds(List<Long> ids) {
+        TypedQuery<Genre> query = entityManager.createQuery("select g from Genre g where g.id in (:ids)", Genre.class);
+        return query.setParameter("ids", ids).getResultList();
+    }
+}
